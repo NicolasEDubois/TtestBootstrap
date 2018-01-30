f557d3a15da08be2ec114a4a7cef32280f9750ac
Nicolas Dubois <nicolas.dubois@uclouvain.be>
2018-01-30 13:27:44 +0100

    Remove comments from Rstudio

--- R/hello.R
+++ R/hello.R
@@ -1,21 +1,4 @@
 #!/usr/bin/Rscript
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#!/usr/bin/Rscript
-
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
 #' Hello Function
 #' @param FirstName A character string corresponding to
 #' the firstname of the person to wil recieve the hello
